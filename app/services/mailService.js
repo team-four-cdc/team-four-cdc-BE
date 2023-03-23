891aa4a76ea874eedff3b7099d7bf06dd951d72a
Mitrais-Faisal <92911544+Mitrais-Faisal@users.noreply.github.com>
2023-03-23 13:18:19 +0700

    Update SMTP configuration (#10)
    * feat: setup config for mailtrap smtp server
    
    * fix: fix object mapping

--- app/services/mailService.js
+++ app/services/mailService.js
@@ -1,31 +1,25 @@
-const nodemailer = require("nodemailer");
+const appRoot = require('app-root-path');
+const smtpConfig = require(`${appRoot}/config/smtpConfig`);
+const nodemailer = require('nodemailer');
 
 class MailService {
   constructor() {}
 
   async sendVerificationEmail({ token, to }) {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD,
-      },
-    });
-
+    const { from, ...config } = smtpConfig;
+    const transporter = nodemailer.createTransport(config);
     const info = await transporter.sendMail({
-      from: process.env.MAIL_USER,
-      to: to,
-      subject: "Account Verification",
-      text: `Verify using this link https://team-four-cdc.vercel.app/verifikasi/${token}`,
+      from: `${from.name} <${from.email}>`,
+      to,
+      subject: 'Account Verification',
+      text: `Verify using this link ${process.env.FE_HOST}/verifikasi/${token}`,
     });
 
-    console.log("Message sent: %s", info.messageId);
+    console.log('Message sent: %s', info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   }
 }

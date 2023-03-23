92e2ee09e45da5bf47b02480b6ce614ac8edfa29
aditya hasry <ryugahasry@gmail.com>
2023-03-23 16:41:04 +0700

    added docker compose

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,16 +2,16 @@ const toBoolean = require("to-bool");
 require("dotenv").config();
 
 const dbConfig = {
-  use_env_variable:
-    process.env.DB_CONN_URI && process.env.DB_CONN_URI.length > 0
-      ? process.env.DB_CONN_URI
-      : undefined,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_TYPE,
+  // use_env_variable:
+  //   process.env.DB_CONN_URI && process.env.DB_CONN_URI.length > 0
+  //     ? process.env.DB_CONN_URI
+  //     : undefined,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  dialect: 'postgres',
   logging: false,
   seederStorage: "sequelize",
 
@@ -25,7 +25,7 @@ const dbConfig = {
   // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
   dialectOptions: {
     encrypt: true,
-    ssl: toBoolean(process.env.DB_SSL),
+    ssl: toBoolean(process.env.PG_SSL),
   },
 };
 

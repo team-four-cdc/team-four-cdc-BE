57501ea48e18e781c6e1a936fe11d90f0679d154
Muhammad Aditya Hasry <Aditya_H765@mitrais.com>
2023-04-05 11:35:03 +0700

    add seeders for default category

---
+++ seeders/20230405025715-create_category.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     *
+     */
+
+    const currentDate = new Date();
+
+    await queryInterface.bulkInsert(
+      'Categories',
+      [
+        {
+          name: 'Teknologi',
+          createdAt: currentDate,
+          updatedAt: currentDate,
+        },
+        {
+          name: 'Masak-masak',
+          createdAt: currentDate,
+          updatedAt: currentDate,
+        },
+        {
+          name: 'Desain UI/UX',
+          createdAt: currentDate,
+          updatedAt: currentDate,
+        },
+        {
+          name: 'Otomotif',
+          createdAt: currentDate,
+          updatedAt: currentDate,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Categories', null, {});
+  },
+};
